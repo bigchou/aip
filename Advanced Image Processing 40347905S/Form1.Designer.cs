590be2aff1689cd436a4c84803b0e92f6b10742c
bigchou <40347905S@gmail.com>
2016-09-19 01:14:30 +0800

    initial

---
+++ Advanced Image Processing 40347905S/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Advanced_Image_Processing_40347905S
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.select_img = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.reset = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.save_img = new System.Windows.Forms.Button();
+            this.copy = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // select_img
+            // 
+            this.select_img.Location = new System.Drawing.Point(176, 395);
+            this.select_img.Name = "select_img";
+            this.select_img.Size = new System.Drawing.Size(105, 23);
+            this.select_img.TabIndex = 0;
+            this.select_img.Text = "Select Image...";
+            this.select_img.UseVisualStyleBackColor = true;
+            this.select_img.Click += new System.EventHandler(this.select_img_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(35, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 317);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(217, 368);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "... ";
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(458, 331);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(75, 23);
+            this.reset.TabIndex = 3;
+            this.reset.Text = "Reset All";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox2.Location = new System.Drawing.Point(553, 37);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(400, 317);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // save_img
+            // 
+            this.save_img.Location = new System.Drawing.Point(724, 394);
+            this.save_img.Name = "save_img";
+            this.save_img.Size = new System.Drawing.Size(75, 23);
+            this.save_img.TabIndex = 5;
+            this.save_img.Text = "Save";
+            this.save_img.UseVisualStyleBackColor = true;
+            this.save_img.Click += new System.EventHandler(this.save_img_Click);
+            // 
+            // copy
+            // 
+            this.copy.Location = new System.Drawing.Point(458, 61);
+            this.copy.Name = "copy";
+            this.copy.Size = new System.Drawing.Size(75, 23);
+            this.copy.TabIndex = 6;
+            this.copy.Text = "HW1";
+            this.toolTip1.SetToolTip(this.copy, "Copy Directly");
+            this.copy.UseVisualStyleBackColor = true;
+            this.copy.Click += new System.EventHandler(this.copy_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 368);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 486);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.copy);
+            this.Controls.Add(this.save_img);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.select_img);
+            this.Name = "Form1";
+            this.Text = "AIP40347905S";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button select_img;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button save_img;
+        private System.Windows.Forms.Button copy;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Advanced Image Processing 40347905S/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.Util;
+using Emgu.CV.Structure;
+
+namespace Advanced_Image_Processing_40347905S
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap beforeBitmap = null;
+        Bitmap afterBitmap = null;
+        bool flag = false;
+
+        private void select_img_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = @"Bitmap文件(*.bmp)|*.bmp|Jpeg文件(*.jpg)|*.jpg|PPM文件(*.ppm)|*.ppm|所有合適文件(*.bmp,*.jpg*.ppm)|*.bmp;*.jpg;*.ppm";
+            openFileDialog1.FilterIndex = 4;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //pictureBox1.Size = new System.Drawing.Size(400, 317);
+                Image<Bgr, Byte> inputImage = new Image<Bgr, byte>(openFileDialog1.FileName);
+                beforeBitmap = inputImage.ToBitmap();
+                pictureBox1.Image = beforeBitmap;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                label1.Text = ("height: "+beforeBitmap.Height.ToString()+"  width: "+beforeBitmap.Width.ToString());
+                label2.Text = Path.GetExtension(openFileDialog1.FileName);
+                flag = true;
+            }
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            label1.Text = "...";
+            label2.Text = "...";
+            pictureBox2.Image = null;
+            flag = false;
+        }
+
+        private void save_img_Click(object sender, EventArgs e)
+        {
+            if (flag)
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "Jpeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+                saveFileDialog1.Title = "Save an Image File";
+                saveFileDialog1.ShowDialog();
+                // If the file name is not an empty string open it for saving.
+                if (saveFileDialog1.FileName != "")
+                {
+                    // Saves the Image via a FileStream created by the OpenFile method.
+                    System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                    // Saves the Image in the appropriate ImageFormat based upon the
+                    // File type selected in the dialog box.
+                    // NOTE that the FilterIndex property is one-based.
+                    switch (saveFileDialog1.FilterIndex)
+                    {
+                        case 1:
+                            this.pictureBox2.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+                        case 2:
+                            this.pictureBox2.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Bmp);
+                            break;
+
+                        case 3:
+                            this.pictureBox2.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Gif);
+                            break;
+                    }
+                    fs.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select an image to continue...");
+            }
+            
+        }
+
+        private void copy_Click(object sender, EventArgs e)
+        {
+            if (flag)
+            {
+                afterBitmap = beforeBitmap;
+                pictureBox2.Image = afterBitmap;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            else
+            {
+                MessageBox.Show("Please select an image to continue...");
+            }
+        }
+    }
+}
